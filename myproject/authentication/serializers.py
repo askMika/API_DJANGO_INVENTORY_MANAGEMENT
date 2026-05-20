6f41a2a81d9212666dcf98d94a1e293e7be44205
askMika <ipelengmorare14@gmail>
2026-05-20 16:54:18 +0200

    minor reviews

--- myproject/authentication/serializers.py
+++ myproject/authentication/serializers.py
@@ -1,33 +1,24 @@
 from rest_framework import serializers
-from .models import User, TeacherProfile, LearnerProfile, CookProfile, LibrarianProfile
+#from django.contrib.auth.models import User
+from .models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
+
     password = serializers.CharField(write_only=True)
-    role = serializers.ChoiceField(choices=User.ROLE_CHOICES)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'role']
-        fields = ['username', 'email', 'password', 'role']
+        fields = ['username', 'email', 'password', 'role','last_name','first_name']
 
     def create(self, validated_data):
-        # Create user with role
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
-            role=validated_data['role']
+            role=validated_data['role'],
+            first_name=validated_data.get('first_name',''),
+            last_name=validated_data.get('last_name','')
         )
 
-        # Auto-create profile depending on role
-        role = validated_data['role']
-        if role == 'teacher':
-            TeacherProfile.objects.create(user=user)
-        elif role == 'learner':
-            LearnerProfile.objects.create(user=user)
-        elif role == 'cook':
-            CookProfile.objects.create(user=user)
-        elif role == 'librarian':
-            LibrarianProfile.objects.create(user=user)
-
         return user
+       
